e1aaf0b0487f807cd7d61ae76c920a597413b7cf
oddlama <oddlama@oddlama.org>
2023-03-02 20:31:52 +0100

    fix: interpret unnamed .config script files as kconfig files (fixes #6)

--- src/script/mod.rs
+++ src/script/mod.rs
@@ -13,19 +13,21 @@ pub trait Script {
     fn apply(&self, bridge: &Bridge) -> Result<()>;
 }
 
-/// Loads the given script file by instanciating
-/// the correct implementation and applys it
+/// Loads the given script file by instanciating the correct implementation
 pub fn load(path: impl AsRef<Path>) -> Result<Box<dyn Script>> {
     let ext = path
         .as_ref()
         .extension()
+        // If no extension is found, interpret the file name as extension.
+        // Happens for files beginning with a . like .config
+        .or_else(|| path.as_ref().file_name())
         .expect("Missing file extension")
         .to_str()
         .unwrap();
 
     Ok(match ext {
         "lua" => Box::new(LuaScript::new(path)?),
-        "txt" | "config" => Box::new(KConfig::new(path)?),
+        "txt" | "config" | ".config" => Box::new(KConfig::new(path)?),
         _ => bail!(format!("Unknown script type {ext}")),
     })
 }
